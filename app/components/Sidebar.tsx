a6ab57726dc673821b600fb06e391a0b6736639f
alibalunn <alibalun666@gmail.com>
2025-10-31 12:22:10 +0300

    Edited sidebar for fonts

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -5,12 +5,12 @@ import { GoPlus } from "react-icons/go";
 
 export default function Sidebar() {
   return (
-    <div className="h-screen border-r border-transparent fixed left-0 top-0 flex flex-col px-4 bg-white w-[300] ring-1 ring-gray-200 shadow-lg">
+    <div className="h-screen border-r border-transparent fixed left-0 top-0 flex flex-col bg-white w-[270px] ring-1 ring-gray-200 shadow-lg px-4">
       <div className="h-20 w-full flex justify-center items-center">
         <span className="font-bold text-lg text-black">Navbar</span>
       </div>
-      <div className="flex-1 w-full p-4 flex flex-col items-center">
-        <Link href="/dashboard/appointments/create" className="flex items-center justify-center gap-2 mt-4 bg-blue-800 text-white px-6 py-3 rounded-sm cursor-pointer w-full">
+      <div className="flex-1 w-full flex flex-col items-center">
+        <Link href="/dashboard/appointments/create" className="flex items-center text-sm justify-center gap-2 mt-4 bg-blue-800 text-white px-8 py-2 rounded-sm cursor-pointer">
           <GoPlus size={24} />
           <span className="text-md">New Appointment</span>
         </Link>
@@ -18,14 +18,14 @@ export default function Sidebar() {
         <div className="mt-6 flex flex-col gap-2 w-full">
           <Link
             href="/dashboard/employees"
-            className="flex items-center text-md text-lg gap-2 px-4 py-2 rounded-md hover:bg-blue-100 hover:text-blue-600 transition-all duration-200"
+            className="flex items-center text-sm text-lg gap-2 px-4 py-2 rounded-md hover:bg-blue-100 hover:text-blue-600 transition-all duration-200"
           >
             <BsPeopleFill />
             <span>Employees</span>
           </Link>
           <Link
             href="/dashboard/customers"
-            className="flex items-center font-medium text-lg hover:text-blue-600 gap-2 px-4 py-2 rounded-md hover:bg-blue-100 hover:text-blue-600 transition-all duration-200"
+            className="flex items-center font-medium text-sm hover:text-blue-600 gap-2 px-4 py-2 rounded-md hover:bg-blue-100 hover:text-blue-600 transition-all duration-200"
           >
             <BsPeople />
             <span>Customers</span>
